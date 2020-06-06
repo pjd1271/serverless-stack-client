7c351c107a22fd93c4276fdc5eac25338f5dbab2
pdipietro <peter.j.dipietro@gmail.com>
2020-06-06 16:03:56 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+	s3: {
+		REGION: 'us-east-1',
+		BUCKET: 'peter-dipietro-notes-app-uploads',
+	},
+	apiGateway: {
+		REGION: 'us-east-1',
+		URL: 'https://v83qmgmub2.execute-api.us-east-1.amazonaws.com/prod',
+	},
+	cognito: {
+		REGION: 'us-east-1',
+		USER_POOL_ID: 'us-east-1_wdRtUCcwL',
+		APP_CLIENT_ID: '10huqq60ev5f8um4jbgpg32s1h',
+		IDENTITY_POOL_ID: 'us-east-1:01ca0901-aae2-4501-a74f-05e36217e8ae',
+	},
+};
